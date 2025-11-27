5bb63b4dcb41e75632327bc20c2fbb5b7f97560a
AnitaLein <anita.leinweber@outlook.de>
2025-11-27 15:58:20 +0100

    import Controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { PlayerModule } from './player/player.module';
 import { BoatsModule } from './boats/boats.module';
 import { AttacksModule } from './attacks/attacks.module';
 import { ImagesModule } from './images/images.module';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [AuthModule, PlayerModule, BoatsModule, AttacksModule, ImagesModule],
-  controllers: [],
+  controllers: [AppController],
   providers: [],
 })
 export class AppModule {}
