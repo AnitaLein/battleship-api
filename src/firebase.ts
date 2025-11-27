f9d3a11580eb5554d6ef0180a5268f6f47cd7b2a
AnitaLein <anita.leinweber@outlook.de>
2025-11-27 15:17:09 +0100

    remove firebase dependency

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,10 +1,7 @@
 import * as admin from 'firebase-admin';
 
-const serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT;
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
-  storageBucket: 'battleship_images',
-});
+admin.initializeApp();
+
 
 export const db = admin.firestore();
 export const bucket = admin.storage().bucket();
