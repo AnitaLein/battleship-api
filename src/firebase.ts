f616ffd6dcce98703fa70a3515adc79219eb3240
AnitaLein <anita.leinweber@outlook.de>
2025-11-28 20:04:57 +0100

    firebase integration

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,12 +1,13 @@
 import * as admin from 'firebase-admin';
 
 const serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT
-
+  
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
-  storageBucket: 'battleship_images',
+  storageBucket: 'battleship_images'
 });
+console.log(serviceAccount);
 
 export const db = admin.firestore();
 export const bucket = admin.storage().bucket();
-export { admin };
+export { admin }
